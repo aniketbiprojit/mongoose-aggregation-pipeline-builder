c796158fc521f4a19716bb8477375456d14a3092
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-05-12 07:59:51 +0530

    fix: prettier

--- src/playground.ts
+++ src/playground.ts
@@ -1,21 +1,21 @@
-import { PipelineStageBuilder } from './index'
+import { PipelineStageBuilder } from "./index";
 /**
  * @description Generate string template from array of aggregation pipeline stages
  * This makes it easier to convert an array of stages into a string template
  */
 
 const build = (_id: string) =>
-	PipelineStageBuilder.fromArray([
-		{
-			$match: {
-				_id: '_id',
-			},
-		},
-		{
-			$project: {
-				_id: 1,
-			},
-		},
-	])
+  PipelineStageBuilder.fromArray([
+    {
+      $match: {
+        _id: "_id",
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+      },
+    },
+  ]);
 
-console.log(build('_id'))
+console.log(build("_id"));
--- src/types.ts
+++ src/types.ts
@@ -1,69 +1,69 @@
-import { PipelineStage } from 'mongoose'
+import { PipelineStage } from "mongoose";
 
 export type PipelineType = {
-	$addFields: PipelineStage.AddFields['$addFields']
+  $addFields: PipelineStage.AddFields["$addFields"];
 
-	$bucket: PipelineStage.Bucket['$bucket']
+  $bucket: PipelineStage.Bucket["$bucket"];
 
-	$bucketAuto: PipelineStage.BucketAuto['$bucketAuto']
+  $bucketAuto: PipelineStage.BucketAuto["$bucketAuto"];
 
-	$collStats: PipelineStage.CollStats['$collStats']
+  $collStats: PipelineStage.CollStats["$collStats"];
 
-	$count: PipelineStage.Count['$count']
+  $count: PipelineStage.Count["$count"];
 
-	$densify: PipelineStage.Densify['$densify']
+  $densify: PipelineStage.Densify["$densify"];
 
-	$fill: PipelineStage.Fill['$fill']
+  $fill: PipelineStage.Fill["$fill"];
 
-	$facet: PipelineStage.Facet['$facet']
+  $facet: PipelineStage.Facet["$facet"];
 
-	$geoNear: PipelineStage.GeoNear['$geoNear']
+  $geoNear: PipelineStage.GeoNear["$geoNear"];
 
-	$graphLookup: PipelineStage.GraphLookup['$graphLookup']
+  $graphLookup: PipelineStage.GraphLookup["$graphLookup"];
 
-	$group: PipelineStage.Group['$group']
+  $group: PipelineStage.Group["$group"];
 
-	$indexStats: PipelineStage.IndexStats['$indexStats']
+  $indexStats: PipelineStage.IndexStats["$indexStats"];
 
-	$limit: PipelineStage.Limit['$limit']
+  $limit: PipelineStage.Limit["$limit"];
 
-	$listSessions: PipelineStage.ListSessions['$listSessions']
+  $listSessions: PipelineStage.ListSessions["$listSessions"];
 
-	$lookup: PipelineStage.Lookup['$lookup']
+  $lookup: PipelineStage.Lookup["$lookup"];
 
-	$match: PipelineStage.Match['$match']
+  $match: PipelineStage.Match["$match"];
 
-	$merge: PipelineStage.Merge['$merge']
+  $merge: PipelineStage.Merge["$merge"];
 
-	$out: PipelineStage.Out['$out']
+  $out: PipelineStage.Out["$out"];
 
-	$planCacheStats: PipelineStage.PlanCacheStats['$planCacheStats']
+  $planCacheStats: PipelineStage.PlanCacheStats["$planCacheStats"];
 
-	$project: PipelineStage.Project['$project']
+  $project: PipelineStage.Project["$project"];
 
-	$redact: PipelineStage.Redact['$redact']
+  $redact: PipelineStage.Redact["$redact"];
 
-	$replaceRoot: PipelineStage.ReplaceRoot['$replaceRoot']
+  $replaceRoot: PipelineStage.ReplaceRoot["$replaceRoot"];
 
-	$replaceWith: PipelineStage.ReplaceWith['$replaceWith']
+  $replaceWith: PipelineStage.ReplaceWith["$replaceWith"];
 
-	$sample: PipelineStage.Sample['$sample']
+  $sample: PipelineStage.Sample["$sample"];
 
-	$search: PipelineStage.Search['$search']
+  $search: PipelineStage.Search["$search"];
 
-	$set: PipelineStage.Set['$set']
+  $set: PipelineStage.Set["$set"];
 
-	$setWindowFields: PipelineStage.SetWindowFields['$setWindowFields']
+  $setWindowFields: PipelineStage.SetWindowFields["$setWindowFields"];
 
-	$skip: PipelineStage.Skip['$skip']
+  $skip: PipelineStage.Skip["$skip"];
 
-	$sort: PipelineStage.Sort['$sort']
+  $sort: PipelineStage.Sort["$sort"];
 
-	$sortByCount: PipelineStage.SortByCount['$sortByCount']
+  $sortByCount: PipelineStage.SortByCount["$sortByCount"];
 
-	$unionWith: PipelineStage.UnionWith['$unionWith']
+  $unionWith: PipelineStage.UnionWith["$unionWith"];
 
-	$unset: PipelineStage.Unset['$unset']
+  $unset: PipelineStage.Unset["$unset"];
 
-	$unwind: PipelineStage.Unwind['$unwind']
-}
+  $unwind: PipelineStage.Unwind["$unwind"];
+};
